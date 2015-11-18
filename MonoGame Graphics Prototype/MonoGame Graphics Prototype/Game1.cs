b5be6d27537aeefedb87809b8fffb20663c760bb
NathanHeadley <Nathanheadley1@gmail.com>
2015-11-18 13:38:40 +0000

    Added Sprite class - not animated yet

---
+++ MonoGame Graphics Prototype/MonoGame Graphics Prototype/Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame_Graphics_Prototype {
+
+    public class Game1 : Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        SpriteFont font;
+        Sprite sprite;
+
+        public Game1() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize() {
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            sprite = new Sprite(Content.Load<Texture2D>("stick"), 50.0f, 50.0f, 0.0f, 3.0f);
+            font = Content.Load<SpriteFont>("basic_font");
+        }
+
+        protected override void UnloadContent() {
+            Content.Unload();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            /*if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();*/
+
+            base.Update(gameTime);
+        }
+
+        // Draws a Sprite from all it's variables information
+        public void drawSprite(Sprite s) {
+            Rectangle scale = new Rectangle((int) s.getPosition().X,
+                                            (int) s.getPosition().Y,
+                                            (int) (s.getWidth() * s.getScale()),
+                                            (int) (s.getHeight() * s.getScale()) );
+            spriteBatch.Draw(s.getTexture(), scale, null, Color.White, s.getRotationRadians(), 
+                s.getOrigin(), SpriteEffects.None, 0);
+        }
+
+        // Draws an animated sprite from all it's variables information
+        //public void drawAnimatedSprite(AnimatedSprite s) {
+            //TO DO: Implement me please
+        //}
+
+        // Uses only the standard font
+        public void drawText(string inText, float inX, float inY, Color inColor) {
+            spriteBatch.DrawString(font, inText, new Vector2(inX, inY), inColor);
+        }
+
+        // For use with different fonts
+        public void drawText(string inText, float inX, float inY, Color inColor, SpriteFont inFont) {
+            spriteBatch.DrawString(inFont, inText, new Vector2(inX, inY), inColor);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            drawSprite(sprite);
+            drawText("Time Elapsed: " + gameTime.TotalGameTime.TotalMilliseconds, 10.0f, 10.0f, Color.Black);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGame Graphics Prototype/MonoGame Graphics Prototype/Sprite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MonoGame_Graphics_Prototype {
+
+    public class Sprite {
+
+        // Variables
+        private Texture2D texture;
+        private Vector2 position,
+                        origin;         // Used for rotations
+        private float rotationDegrees,
+                    scale;
+
+        // Initialiser(s)
+        public Sprite(Texture2D inTexture, float inX, float inY,
+            float inRotation = 0, float inScale = 1, float inOriginX = -1, float inOriginY = -1) {
+
+            texture = inTexture;
+            position.X = inX;
+            position.Y = inY;
+            if (origin.X == -1) {
+                origin.X = inX + (texture.Width / 2);
+                origin.Y = inY + (texture.Height / 2);
+            }
+            rotationDegrees = inRotation;
+            scale = inScale;
+        }
+
+
+        // Getters
+        public Texture2D getTexture() { return texture; }
+
+        public Vector2 getPosition() { return position; }
+
+        public Vector2 getOrigin() { return origin; }
+
+        public float getRotationDegrees() { return rotationDegrees; }
+
+        public float getRotationRadians()
+        {
+            float radians = rotationDegrees * (3.1415f / 180);
+            return radians;
+        }
+
+        public float getScale() { return scale; }
+
+        public int getWidth() { return texture.Width; }
+
+        public int getHeight() { return texture.Height; }
+
+        // Setters
+        public void setTexture(Texture2D inTexture) {
+            texture = inTexture;
+        }
+        public void setPosition(Vector2 inPosition) {
+            position = inPosition;
+        }
+        public void setRotation(float inDegrees) {
+            rotationDegrees = inDegrees;
+        }
+
+    }
+}
