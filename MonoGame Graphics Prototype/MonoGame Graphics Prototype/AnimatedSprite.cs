285fc0b556624bb9c3893e27bc0435e4256f2d0d
NathanHeadley <Nathanheadley1@gmail.com>
2015-11-20 11:53:57 +0000

    Added various comments for readability yo.

--- MonoGame Graphics Prototype/MonoGame Graphics Prototype/AnimatedSprite.cs
+++ MonoGame Graphics Prototype/MonoGame Graphics Prototype/AnimatedSprite.cs
@@ -21,17 +21,18 @@ namespace MonoGame_Graphics_Prototype {
 
     public class AnimatedSprite : Sprite {
 
-        //Variables
-        int frameWidth,
-            frameHeight,
-            horizontalFrames,
-            verticalFrames,
-            currentHorizontal,
-            currentVertical,
-            currentState;
-        double frameTime,
-                lastTime;
+        // Variables
+        int frameWidth,         // Width of each frame in pixels
+            frameHeight,        // Height of each frame in pixels
+            horizontalFrames,   // Number of frames in each row
+            verticalFrames,     // Number of rows or vertical frames
+            currentHorizontal,  // Current horizontal frame
+            currentVertical,    // Current vertical frame (current animation)
+            currentState;       // Current state of sprite (0 = still, 1 = animating)
+        double frameTime,       // Interval between each frame in milliseconds (1000ms = 1s)
+                lastTime;       // The time that the last animation occurred
 
+        // Initializer
         public AnimatedSprite(Texture2D inSpriteSheet, float inX, float inY, 
             int inWidth, int inHeight, double inTime, float inScale = 1) {
             
@@ -45,6 +46,7 @@ namespace MonoGame_Graphics_Prototype {
             currentState = 1;
         }
 
+        // Method to change which frame is displaying
         public void changeFrame(double inTime) {
             if (currentHorizontal < horizontalFrames - 1) {
                 currentHorizontal++;
@@ -54,12 +56,13 @@ namespace MonoGame_Graphics_Prototype {
             lastTime = inTime;
         }
 
+        // Method to start animating, takes in which animation to start
         public void startAnimation(int inAnimation) {
             currentVertical = inAnimation;
             currentState = 1;
         }
 
-        //Getters - no need for setters that i've found?
+        // Getters - no need for setters that i've found?
         public int getFrameWidth() {
             return frameWidth;
         }
